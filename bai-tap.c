bfa1596f9a8be7b23cd1c1500d16debbc3569c77
chusonIT23 <149054509+chusonIT23@users.noreply.github.com>
2023-11-02 13:29:04 +0700

    Add files via upload

---
+++ bai-tap.c
@@ -0,0 +1,15 @@
+bai tap 1 
+Ket qua cua chuong trình trên là:
+
+Giá tri cua a se  là 16
+Mot chuoi "Tong cua a và b là: 25" se xuut hien trên màn hình hien thi.
+
+bai tap 2
+Ket qua cua doan chuong trình trên se là:
+
+Giá tri cua bien a ban dau là 7.
+Sau khi thuc hien a++, giá tri caa a tang lên 1 và tro thành 8.
+Sau dó, giá tri cua a duoc cap nhat bang cong voi giá tri cua b--, tuc là a = 8 + 8 = 16. Lúc này, giá tr? c?a a là 16 và giá tr? c?a b gi?m xu?ng còn 7.
+Tiep theo, bien b duoc giam di 1, tuc là b = 7 - 1 = 6.
+Cuoi cùng, giá tri cua a duoc giam di 1, tuc là a = 16 - 1 = 15.
+Vay, ket qua cuoi cùng cua doan chuong trình là a = 15 và b = 6.
